1202f73999ef914641ddb847a85b9700d2262257
Khalid-MahmouD <40337849+Khalid-MahmouD@users.noreply.github.com>
2020-02-14 01:45:07 +0200

    Update CF53A.cpp

--- CF/CF53A.cpp
+++ CF/CF53A.cpp
@@ -1,7 +1,40 @@
+/*
+		URL  : 
+		Author : Khalid Mahmoud
+*/
 #include<iostream>
+#include<algorithm>
+#include<vector>
 using namespace std;
-
+int n;
+string s1[105];
+vector<string>v;
 int main() {
+	string s;
+	cin >> s >> n;
+	for (int i = 0; i < n; ++i) {
+		cin >> s1[i];
+		if (s1[i] == s) {
+			cout << s << "\n";
+			return 0;
+		}
+	}
+	bool flag = 0;
+	for (int j = 0; j < n; ++j) {
+		for (int i = 0;i < s.size();++i) {
+			if (s1[j][i] == s[i])
+				flag = 1;
+			else {
+				flag = 0;
+				break;
+			}
+		}
+		if (flag)
+			v.push_back(s1[j]);
+	}
+	if (v.empty()) { cout << s << endl; return 0; }
+	sort(v.begin(), v.end());
+	cout << v[0] << endl;
 
-	return 0;
+   return 0;
 }
